1793b5dbfd7155226e01d66e405da16013205222
goushikun6021003 <496704144@qq.com>
2020-09-21 15:38:14 +0800

    封装构造数据函数

--- pkg/controllers/mail.go
+++ pkg/controllers/mail.go
@@ -1,21 +1,33 @@
 package controllers
 
 import (
+	"encoding/json"
 	"log"
 	"strings"
-	"encoding/json"
-
-	"github.com/go-gomail/gomail"
 
 	"github.com/goushikun6021003/kube-local/pkg/model"
+	"gopkg.in/gomail.v2"
 )
 
+func MailSetter(m *gomail.Message, SendToEmails []string, user string, title string, text string) *gomail.Message {
+	// 收件人,...代表打散列表填充不定参数
+	m.SetHeader("To", SendToEmails...)
+	// 发件人
+	m.SetAddressHeader("From", user, title)
+	// 主题
+	m.SetHeader("Subject", title)
+	// 正文
+	m.SetBody("text/html", text)
+
+	return m
+}
+
 // SendEmail
-func SendEmail( ruler *model.Ruleser ) string {
+func SendEmail(ruler *model.Ruleser) string {
 	// 转化发送内容为json格式
 	rulerJson, err := json.Marshal(ruler)
 	if err != nil {
-		log.Println("[email]",err.Error())
+		log.Println("[email]", err.Error())
 	}
 	// 读取Model中的结构体
 	mail := model.Config.Mail
@@ -26,7 +38,6 @@ func SendEmail( ruler *model.Ruleser ) string {
 		return "eamil未配置未开启状态,请先配置open-email为1"
 	}
 
-
 	//Emails= xxx1@qq.com,xxx2@qq.com,xxx3@qq.com
 	SendToEmails := []string{}
 	m := gomail.NewMessage()
@@ -36,21 +47,16 @@ func SendEmail( ruler *model.Ruleser ) string {
 	for _, Email := range strings.Split(mail.Emails, ",") {
 		SendToEmails = append(SendToEmails, strings.TrimSpace(Email))
 	}
-	// 收件人,...代表打散列表填充不定参数
-	m.SetHeader("To", SendToEmails...)
-	// 发件人
-	m.SetAddressHeader("From", mail.User, mail.Title)
-	// 主题
-	m.SetHeader("Subject", mail.Title)
-	// 正文
-	m.SetBody("text/html", string(rulerJson))
+
+	m = MailSetter(m, SendToEmails, mail.User, mail.Title, string(rulerJson))
+
 	d := gomail.NewDialer(mail.Host, mail.Port, mail.User, mail.Password)
 	// 发送
 	err = d.DialAndSend(m)
 	model.AlertToCounter.WithLabelValues("email", string(rulerJson), mail.Emails).Add(1)
 	if err != nil {
-		log.Println( "[email]", err.Error())
+		log.Println("[email]", err.Error())
 	}
-	log.Println( "[email]", "email send ok to " + mail.Emails)
+	log.Println("[email]", "email send ok to "+mail.Emails)
 	return "email send ok to " + mail.Emails
 }
